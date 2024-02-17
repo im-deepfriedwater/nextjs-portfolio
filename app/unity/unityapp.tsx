c69c5d6b5e15924e8b3031f9f5da4f85fb987f9f
Justin Torres <deepfriedwater1@gmail.com>
2024-02-16 18:18:36 -0800

    rename file path to make it easier for imports, update with new unity builds with new icons for sound and updated filter settings for the next prompt icon

--- app/unity/unityapp.tsx
+++ app/unity/unityapp.tsx
@@ -5,10 +5,10 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const UnityApp = () => {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/build/build.loader.js",
-    dataUrl: "/build/build.data",
-    frameworkUrl: "/build/build.framework.js",
-    codeUrl: "/build/build.wasm",
+    loaderUrl: "/build/public.loader.js",
+    dataUrl: "/build/public.data",
+    frameworkUrl: "/build/public.framework.js",
+    codeUrl: "/build/public.wasm",
   });
 
   return <Unity unityProvider={unityProvider} style={{ width: 1366, height: 768, }} />;
